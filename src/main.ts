65b0735f44bf77450b888232635cc4f55dc3e646
Oleh <tsuria.dev@gmail.com>
2023-10-15 23:44:40 +0300

    initialized project

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './AppModule';
+import * as process from "process";
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = parseInt(process.env.PORT) || 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Autolab')
+    .setDescription('The Autolab API description')
+    .setVersion('1.0')
+    .addTag('Autolab')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port);
+
+  console.log(`Server start on port: ${port}`);
+}
+bootstrap();
